e41e321a86c82b259f0434ee4081fc4b287aac44
johannao62 <56741552+johannao62@users.noreply.github.com>
2023-03-26 17:45:20 -0500

    Lógica básica con Alert que indica si el loguin es correcto.

--- app/src/main/java/com/miempresa/demoapp/MainActivity.java
+++ app/src/main/java/com/miempresa/demoapp/MainActivity.java
@@ -2,13 +2,41 @@ package com.miempresa.demoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usuario;
+    private EditText clave;
+    private String claveCorrecta = "estaEs";
+    private String usuarioCorrecto = "Jo@gmail.com";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usuario = findViewById(R.id.edtMail);
+        clave = findViewById(R.id.edtPassword);
+
     }
+
+    public void AutenticaUsuario(View v){
+
+        if (usuario.getText().toString().equalsIgnoreCase(usuarioCorrecto)
+                && clave.getText().toString().equals(claveCorrecta)
+        ){
+            //Se muestra un mensaje emergente de bienvenida
+            Toast.makeText(this, "Bienvenido a la My App Android", Toast.LENGTH_LONG).show();
+
+        } else {
+            Toast.makeText(this, "Usuario o Contraseña incorrecta", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
 }
