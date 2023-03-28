fe2cee6cee2def3e9106b99462f6e5b9f8071bec
johannao62 <56741552+johannao62@users.noreply.github.com>
2023-03-27 23:53:24 -0500

    Se modifica el formulario de forma más básica y se logra cargar el método en el botón, cambiando el error que aparecía antes.

--- app/src/main/java/com/miempresa/demoapp/RegistroActivity.java
+++ app/src/main/java/com/miempresa/demoapp/RegistroActivity.java
@@ -3,12 +3,52 @@ package com.miempresa.demoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegistroActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView dropdownTipoDoc, dropdownDepartamento, dropdownProvincia, dropdownDistrito;
+    private EditText edtNameUser, edtNumDocU, edtTelefonoU,
+            edtDireccionU, edtPasswordUser, edtEmailUser;
+    private TextInputLayout txtInputNameUser, txtInputApellidoPaternoU, txtInputApellidoMaternoU,
+            txtInputTipoDoc, txtInputNumeroDocU, txtInputDepartamento, txtInputProvincia,
+            txtInputDistrito, txtInputTelefonoU, txtInputDireccionU, txtInputEmailUser, txtInputPasswordUser;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+
+        edtNameUser = findViewById(R.id.edtNameUser);
+        edtNumDocU = findViewById(R.id.edtNumDocU);
+        edtTelefonoU = findViewById(R.id.edtTelefonoU);
+        edtDireccionU = findViewById(R.id.edtDireccionU);
+        edtPasswordUser = findViewById(R.id.edtPasswordUser);
+        edtEmailUser = findViewById(R.id.edtEmailUser);
+        //AutoCompleteTextView
+        dropdownTipoDoc = findViewById(R.id.dropdownTipoDoc);
+        dropdownDepartamento = findViewById(R.id.dropdownDepartamento);
+        dropdownProvincia = findViewById(R.id.dropdownProvincia);
+        //TextInputLayout
+        txtInputNameUser = findViewById(R.id.txtInputNameUser);
+        txtInputTipoDoc = findViewById(R.id.txtInputTipoDoc);
+        txtInputNumeroDocU = findViewById(R.id.txtInputNumeroDocU);
+        txtInputDepartamento = findViewById(R.id.txtInputDepartamento);
+        txtInputProvincia = findViewById(R.id.txtInputProvincia);
+        txtInputTelefonoU = findViewById(R.id.txtInputTelefonoU);
+        txtInputDireccionU = findViewById(R.id.txtInputDireccionU);
+        txtInputEmailUser = findViewById(R.id.txtInputEmailUser);
+        txtInputPasswordUser = findViewById(R.id.txtInputPasswordUser);
+    }
+
+    public void CargarFormulario(View v){
+        float num1=0f;
+        float num2=0f;
+        System.out.println("Se ingresa a cargar formulario");
     }
 }
